3001a33c14307bebb790ccb7996147207ffeb204
Xander Cole <65436489+Clackroe@users.noreply.github.com>
2021-05-03 18:06:50 -0400

    Added function prototypes

--- Project#3_AlexanderCole.cpp
+++ Project#3_AlexanderCole.cpp
@@ -1,3 +1,48 @@
 #include <iostream>
 #include <iomanip>
 using namespace std;
+
+// Function prototypes
+void displayTitle();
+string getResolutionString(int);
+double getMultiplier(int);
+double calculatePerformanceScore(int, int, int, double);
+string getRecommendedQuality(double);
+void displayInfo(int, int, int, string, double, string);
+
+//-------------------------------------------------------------------------------------------------------------
+
+int main()
+{
+	
+} //Main Function end
+
+void displayTitle()
+{
+	
+}
+
+string getResolutionString(int)
+{
+	
+}
+
+double getMultiplier(int)
+{
+	
+}
+
+double calculatePerformanceScore(int, int, int, double)
+{
+	
+}
+
+string getRecommendedQuality(double)
+{
+	
+}
+
+void displayInfo(int, int, int, string, double, string)
+{
+	
+}
